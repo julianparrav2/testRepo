80357e7e65dd47d76f5a9e96c7e048878c76ceca
jennymgarces <48691195+jennymgarces@users.noreply.github.com>
2019-08-02 15:06:13 -0500

    ya se genero el numero al azar

---
+++ src/main/java/edu/upb/testrepo/Juego.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.upb.testrepo;
+
+import java.util.Random;
+import java
+
+/**
+ *
+ * @author informatica
+ */
+public class Juego {
+    
+    public static void main(string[]args){
+        
+        System.out.println("dame un numero de 1 al 20");
+        Scanner teclado = new Scanner(System.in);
+        int numero = Integer.parseInt(teclado.nextLine());
+        
+        Random r = new Random();
+        int numeroAletorio = r.nextInt(20)+1;
+        
+        if(numero == numeroAletorio){
+            
+        
+                
+    
+        
+    
+    }
+        
+     
+                
+                
+                
+                
+               
+             
+                
+    }
+}
---
+++ src/main/java/edu/upb/testrepo/scanener.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.upb.testrepo;
+
+import java.io.InputStream;
+
+/**
+ *
+ * @author informatica
+ */
+public class scanener {
+
+    public scanener(InputStream in) {
+    }
+    
+}
